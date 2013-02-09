245f0459f030512f3a848124b437215143c11803
Claudio Beatrice <claudi0.beatric3@gmail.com>
2013-02-09 20:01:44 +0100

    added rgbToHex and url functions, improved round

--- underscore.silent.js
+++ underscore.silent.js
@@ -54,10 +54,37 @@ _.mixin({
    *
    * @return Number
    */
-  round : function (number, digits) {
-      var multiple = Math.pow(10, digits);
+  round : function (amount, total, scale, decimalDigits) {
+    if (total === 0) {
+        return 0;
+    }
 
-      return Math.round(number * multiple) / multiple;
+    amount = amount || 0;
+    scale = scale || 1;
+    decimalDigits = decimalDigits || 0;
+
+    var precisionMult = Math.pow(10, decimalDigits);
+
+    return Math.round(precisionMult * scale * (amount / total)) / precisionMult;
+  },
+
+  /**
+   * transform a number from rgb to hex
+   *
+   * @var String rgb
+   */
+  rgbToHex : function(rgb) {
+    if (!rgb) {
+      return '#FFFFFF'; //default color
+    }
+
+    var hex_rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+
+    function hex(x) {
+      return ("0" + parseInt(x, 10).toString(16)).slice(-2);
+    }
+
+    return hex_rgb ? ("#" + hex(hex_rgb[1]) + hex(hex_rgb[2]) + hex(hex_rgb[3])) : rgb;
   },
 
   /**
@@ -69,5 +96,16 @@ _.mixin({
    */
   isVoid : function (value) {
     return _.isNull(value) || _.isUndefined(value);
+  },
+
+  /**
+   * Returns an URL given the path
+   *
+   * @var string path the relative path
+   *
+   * @return string the absolute path
+   */
+  url : function (path) {
+    return location.protocol + '//' + location.host + "/" + _.ltrim(path, "/");
   }
 });
