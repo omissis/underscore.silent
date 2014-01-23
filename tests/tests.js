89e8840ccdef415cca6c8675f71d362b410ec423
omissis <claudi0.beatric3@gmail.com>
2014-01-23 16:29:52 +0100

    added require() and getGlobalObject() functions

--- tests/tests.js
+++ tests/tests.js
@@ -1,60 +1,114 @@
+QUnit.module("Underscore Silent", {
+    teardown: function () {
+        var globj = _.getGlobalObject();
+
+        delete globj.Foo;
+    },
+});
+
 test("ns single level", function () {
-  _.ns("Foo");
-  deepEqual(Foo, {});
+    _.ns("Foo");
+    deepEqual(Foo, {});
 });
 
 test("ns multiple level", function () {
-  _.ns("Foo.Bar.Baz");
-  deepEqual(Foo.Bar.Baz, {});
+    _.ns("Foo.Bar.Baz");
+    deepEqual(Foo.Bar.Baz, {});
 
-  _.ns("Foo.Bar");
-  deepEqual(Foo.Bar, {Baz : {}});
+    _.ns("Foo.Bar");
+    deepEqual(Foo.Bar, {Baz : {}});
 });
 
 test("sum", function () {
-  equal(_.sum([1, 2, 3]), 6);
-  equal(_.sum([1, 2, -3]), 0);
-  equal(_.sum([{ value : 1}, { value : 2 }, { value : 3 }], function (item) { return item.value; }), 6);
+    equal(_.sum([1, 2, 3]), 6);
+    equal(_.sum([1, 2, -3]), 0);
+    equal(_.sum([{ value : 1}, { value : 2 }, { value : 3 }], function (item) { return item.value; }), 6);
 });
 
 test("replace", function () {
-  equal(_.replace('foo %{token} baz', {token : "bar"}), "foo bar baz");
-  equal(_.replace('foo baz',          {token : "bar"}), "foo baz");
+    equal(_.replace('foo %{token} baz', { token : "bar" }), "foo bar baz");
+    equal(_.replace('foo baz',          { token : "bar" }), "foo baz");
 });
 
 test("percent", function () {
-  equal(_.percent(1, 2), 1);
-  equal(_.percent(4, 4), 1);
-  equal(_.percent(5, 5, 2), 2);
-  equal(_.percent(5, 15, 1, 2), 0.33);
-  equal(_.percent(5, 15, 1, 4), 0.3333);
-  equal(_.percent(10, 15, 1, 6), 0.666667);
+    equal(_.percent(1, 2), 1);
+    equal(_.percent(4, 4), 1);
+    equal(_.percent(5, 5, 2), 2);
+    equal(_.percent(5, 15, 1, 2), 0.33);
+    equal(_.percent(5, 15, 1, 4), 0.3333);
+    equal(_.percent(10, 15, 1, 6), 0.666667);
 });
 
 test("round", function () {
-  equal(_.round(1.2356, 0), 1);
-  equal(_.round(1.2356, 1), 1.2);
-  equal(_.round(1.2356, 2), 1.24);
-  equal(_.round(1.2356, 3), 1.236);
+    equal(_.round(1.2356, 0), 1);
+    equal(_.round(1.2356, 1), 1.2);
+    equal(_.round(1.2356, 2), 1.24);
+    equal(_.round(1.2356, 3), 1.236);
 });
 
 test("rgbToHex", function () {
-  equal(_.rgbToHex(),                 '#ffffff');
-  equal(_.rgbToHex('rgb(0, 0, 0)'),   '#000000');
-  equal(_.rgbToHex('rgb(255, 0, 0)'), '#ff0000');
-  equal(_.rgbToHex('rgb(0, 255, 0)'), '#00ff00');
-  equal(_.rgbToHex('rgb(0, 0, 255)'), '#0000ff');
+    equal(_.rgbToHex(),                 '#ffffff');
+    equal(_.rgbToHex('rgb(0, 0, 0)'),   '#000000');
+    equal(_.rgbToHex('rgb(255, 0, 0)'), '#ff0000');
+    equal(_.rgbToHex('rgb(0, 255, 0)'), '#00ff00');
+    equal(_.rgbToHex('rgb(0, 0, 255)'), '#0000ff');
 });
 
 test("isVoid", function () {
-  equal(_.isVoid(null),      true, "null is void");
-  equal(_.isVoid(undefined), true, "undefined is void");
-  equal(_.isVoid(""),        false, "empty string is not void");
-  equal(_.isVoid(0),         false, "number zero is not void");
-  equal(_.isVoid([]),        false, "empty array is not void");
-  equal(_.isVoid({}),        false, "empty object is not void");
+    equal(_.isVoid(null),      true, "null is void");
+    equal(_.isVoid(undefined), true, "undefined is void");
+    equal(_.isVoid(""),        false, "empty string is not void");
+    equal(_.isVoid(0),         false, "number zero is not void");
+    equal(_.isVoid([]),        false, "empty array is not void");
+    equal(_.isVoid({}),        false, "empty object is not void");
 });
 
 test("url", function () {
-  equal(_.url("foo_bar"), location.protocol + '//' + location.host + "/foo_bar");
+    var globj = _.getGlobalObject();
+
+    if (_.isUndefined(globj.location)) {
+        globj.location = {
+            protocol : 'http',
+            host : 'localhost'
+        };
+    }
+
+    equal(_.url("foo_bar"), location.protocol + '://' + location.host + "/foo_bar");
+
+    delete globj.location;
+});
+
+test("require", function () {
+    var globj = _.getGlobalObject();
+
+    throws(
+        function () {
+            _.require('Foo', function (Foo) {});
+        },
+        "Foo object does not exist."
+    );
+
+    throws(
+        function () {
+            _.require(['Foo'], function (Foo) {});
+        },
+        "Foo object does not exist."
+    );
+
+    globj.Foo = { bar: 'baz' };
+
+    deepEqual(_.require(['Foo'], function (Foo) {
+        return Foo;
+    }), Foo);
+
+    delete globj.Foo;
+
+    globj.Foo = { bar: 'baz' };
+    globj.Bar = { baz: 'quux' };
+
+    deepEqual(_.require(['Foo', 'Bar'], function (Foo, Bar) {
+        return [Foo, Bar];
+    }), [Foo, Bar]);
+
+    delete globj.Bar;
 });
--- underscore.silent.js
+++ underscore.silent.js
@@ -1,153 +1,202 @@
-//  Underscore.silent
-//  (c) 2011-2012 Claudio Beatrice <claudi0 d0t beatric3 at gmail d0t c0m>.
-//  Underscore.silent is freely distributable under the terms of the MIT license.
-//  Documentation: https://github.com/omissis/underscore.silent
-//  Version '0.1.3'
+//    Underscore.silent
+//    (c) 2011-2014 Claudio Beatrice <claudi0 d0t beatric3 at gmail d0t c0m>.
+//    Underscore.silent is freely distributable under the terms of the MIT license.
+//    Documentation: https://github.com/omissis/underscore.silent
+//    Version '0.1.4'
 
 (function (_) {
-  "use strict";
+    "use strict";
 
-  _.mixin({
-    /**
-     * Create a hierarchy of nested objects to simulate a namespace.
-     *
-     * Beside helping to avoid to pollute the global namespace, this function also ensure the
-     * wanted tree of objects exists, otherwise it creates it.
-     *
-     * @var string namespace
-     *   a string describing the namespace, such as: Foo.Bar.Baz
-     */
-    ns : function (namespace) {
-      var win = window;
+    _.mixin({
+        /**
+         * Get the global object in the current execution context
+         *
+         * It could be window in a browser or global when running node.js.
+         *
+         * @return Object|Null
+         */
+        getGlobalObject : function () {
+            return (function () {
+                return this || (1,eval)('this');
+            }());
+        },
 
-      namespace = namespace.split('.');
+        /**
+         * Create a hierarchy of nested objects to simulate a namespace.
+         *
+         * Beside helping to avoid to pollute the global namespace, this function also ensure the
+         * wanted tree of objects exists, otherwise it creates it.
+         *
+         * @var String namespace
+         *     a string describing the namespace, such as: Foo.Bar.Baz
+         */
+        ns : function (namespace) {
+            var GLOBAL = _.getGlobalObject();
 
-      for (var i = 0, len = namespace.length; i < len; ++i) {
-        if (undefined === win[namespace[i]]) {
-          win[namespace[i]] = {};
+            namespace = namespace.split('.');
+
+            for (var i = 0, len = namespace.length; i < len; i += 1) {
+                if (_.isUndefined(GLOBAL[namespace[i]])) {
+                    GLOBAL[namespace[i]] = {};
+                }
+                GLOBAL = GLOBAL[namespace[i]];
+            }
+        },
+
+        /**
+         * Invokes the given callback passing the specified dependencies.
+         *
+         * @var String|Array moduleNames
+         * @var Function callback
+         *
+         * @return Object|Null
+         */
+        require : function (moduleNames, callback) {
+            var modules = [];
+            var GLOBAL = _.getGlobalObject();
+            var getModule = function (module) {
+                if (_.isUndefined(GLOBAL[module])) {
+                    throw module.toString() + " object does not exist.";
+                }
+
+                return GLOBAL[module];
+            };
+
+            if (_.isArray(moduleNames)) {
+                for (var i = 0, len = moduleNames.length; i < len; i += 1) {
+                    modules.push(getModule(moduleNames[i]));
+                }
+            }
+
+            if (_.isString(moduleNames)) {
+                modules.push(getModule(moduleNames));
+            }
+
+            return callback.apply(GLOBAL, modules);
+        },
+
+        /**
+         * Calculate sum of an array.
+         *
+         * @var Array obj
+         *     array of which you want to calculate the sum
+         * @var Function iterator
+         *     generates the criterion by which the value is ranked.
+         *
+         * @return Number
+         */
+        sum : function (obj, iterator) {
+            if (!iterator && _.isEmpty(obj)) {
+                return -Infinity;
+            }
+
+            var total = 0;
+
+            for (var i = 0, len = obj.length; i < len; i += 1) {
+                total += iterator ? iterator(obj[i]) : obj[i];
+            }
+
+            return total;
+        },
+
+        /**
+         * Replace tokens in a string with the corresponding values in the given map.
+         *
+         * Tokens in the string must be in the %{token} form.
+         *
+         * @var String str
+         *     a string with the values to be replaced
+         * @var String map
+         *     an object containing the values to replace in the string,
+         *     keyed as the tokens in the string
+         *
+         * @return String
+         */
+        replace : function (str, map) {
+            return str.replace(/\%\{(\w+)\}/g, function(match, key) {
+                return map[key];
+            });
+        },
+
+        /**
+         * Calculate the percentage of an amount on a total.
+         *
+         * @var Number amount
+         * @var Number total
+         * @var Number scale
+         * @var Number decimalDigits
+         *
+         * @return Number
+         */
+        percent : function (amount, total, scale, decimalDigits) {
+            if (total === 0) {
+                return 0;
+            }
+
+            amount = amount || 0;
+            scale = scale || 1;
+            decimalDigits = decimalDigits || 0;
+
+            var precisionMult = Math.pow(10, decimalDigits);
+
+            return Math.round(precisionMult * scale * (amount / total)) / precisionMult;
+        },
+
+        /**
+         * Rounds a number to the needed digit.
+         *
+         * @var Number number
+         * @var Number digits
+         *
+         * @return Number
+         */
+        round : function (number, digits) {
+            var multiple = Math.pow(10, digits);
+
+            return Math.round(number * multiple) / multiple;
+        },
+
+        /**
+         * Transform a number from rgb to hex.
+         *
+         * @var String rgb
+         */
+        rgbToHex : function(rgb) {
+            if (!rgb) {
+                return '#ffffff'; //default color
+            }
+
+            var hex_rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+
+            function hex(x) {
+                return ("0" + parseInt(x, 10).toString(16)).slice(-2);
+            }
+
+            return hex_rgb ? ("#" + hex(hex_rgb[1]) + hex(hex_rgb[2]) + hex(hex_rgb[3])) : rgb;
+        },
+
+        /**
+         * Returns true if the value of object is Null or Undefined.
+         *
+         * @var String value
+         *
+         * @return Boolean
+         */
+        isVoid : function (value) {
+            return _.isNull(value) || _.isUndefined(value);
+        },
+
+        /**
+         * Returns an URL given the path
+         *
+         * @var String path the relative path
+         *
+         * @return String the absolute path
+         */
+        url : function (path) {
+            var GLOBAL = _.getGlobalObject();
+
+            return GLOBAL.location.protocol + '://' + GLOBAL.location.host + "/" + path.replace(/\/{1,}$/, '');
         }
-        win = win[namespace[i]];
-      }
-    },
-
-    /**
-     * Calculate sum of an array
-     *
-     * @var Array obj
-     *   array of which you want to calculate the sum
-     * @var function iterator
-     *   generates the criterion by which the value is ranked.
-     *
-     * @return Number
-     */
-    sum : function (obj, iterator) {
-      if (!iterator && _.isEmpty(obj)) return -Infinity;
-
-      var total = 0;
-
-      for (var i = 0, len = obj.length; i < len; ++i) {
-        total += iterator ? iterator(obj[i]) : obj[i];
-      }
-
-      return total;
-    },
-
-    /**
-     * Replace tokens in a string with the corresponding values in the given map.
-     *
-     * Tokens in the string must be in the %{token} form.
-     *
-     * @var string str
-     *   a string with the values to be replaced
-     * @var string map
-     *   an object containing the values to replace in the string,
-     *   keyed as the tokens in the string
-     *
-     * @return string
-     */
-    replace : function (str, map) {
-      return str.replace(/\%\{(\w+)\}/g, function(match, key) {
-        return map[key];
-      });
-    },
-
-    /**
-     * Calculate the percentage of an amount on a total
-     *
-     * @var Number amount
-     * @var Number total
-     * @var Number scale
-     * @var Number decimalDigits
-     *
-     * @return Number
-     */
-    percent : function (amount, total, scale, decimalDigits) {
-      if (total === 0) {
-        return 0;
-      }
-
-      amount = amount || 0;
-      scale = scale || 1;
-      decimalDigits = decimalDigits || 0;
-
-      var precisionMult = Math.pow(10, decimalDigits);
-
-      return Math.round(precisionMult * scale * (amount / total)) / precisionMult;
-    },
-
-    /**
-     * Rounds a number to the needed digit
-     *
-     * @var Number number
-     * @var Number digits
-     *
-     * @return Number
-     */
-    round : function (number, digits) {
-      var multiple = Math.pow(10, digits);
-
-      return Math.round(number * multiple) / multiple;
-    },
-
-    /**
-     * transform a number from rgb to hex
-     *
-     * @var String rgb
-     */
-    rgbToHex : function(rgb) {
-      if (!rgb) {
-        return '#ffffff'; //default color
-      }
-
-      var hex_rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-
-      function hex(x) {
-        return ("0" + parseInt(x, 10).toString(16)).slice(-2);
-      }
-
-      return hex_rgb ? ("#" + hex(hex_rgb[1]) + hex(hex_rgb[2]) + hex(hex_rgb[3])) : rgb;
-    },
-
-    /**
-     * Returns true if the value of object is Null or Undefined.
-     *
-     * @var string value
-     *
-     * @return boolean
-     */
-    isVoid : function (value) {
-      return _.isNull(value) || _.isUndefined(value);
-    },
-
-    /**
-     * Returns an URL given the path
-     *
-     * @var string path the relative path
-     *
-     * @return string the absolute path
-     */
-    url : function (path) {
-      return location.protocol + '//' + location.host + "/" + path.replace(/\/{1,}$/, '');
-    }
-  });
+    });
 }(_));
